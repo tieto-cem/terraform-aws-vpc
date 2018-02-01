daec3243c3dd194247c592e6994ed6ace84fdc54
timotapanainen@users.noreply.github.com <timo.tapanainen@gmail.com>
2018-02-01 20:38:55 +0200

    use relative references in examples

--- example/detailed/main.tf
+++ example/detailed/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source                   = "github.com/tieto-cem/terraform-aws-vpc?ref=v0.1.1"
+  source                   = "../.."
   name_prefix              = "detailed-example"
   azs                      = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   cidr                     = "10.0.0.0/16"
--- example/simple/main.tf
+++ example/simple/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_availability_zones" "azs" {}
 
 module "vpc" {
-  source      = "github.com/tieto-cem/terraform-aws-vpc?ref=v0.1.1"
+  source      = "../.."
   name_prefix = "simple-example"
   cidr        = "10.0.0.0/16"
   azs         = "${data.aws_availability_zones.azs.names}"
