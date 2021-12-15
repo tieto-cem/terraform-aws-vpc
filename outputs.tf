57632d5549813e43e8cd8af2838b5ceac26b9ef6
Mika Riekkinen <mika.riekkinen@gmail.com>
2021-12-15 06:59:38 +0200

    Minimum viable changes for Terraform 1.1

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 
 output "id" {
   description = "VPC id"
-  value = "${aws_vpc.vpc.id}"
+  value = aws_vpc.vpc.id
 }
 
 output "private_subnet_ids" {
   description = "List of private subnet IDs"
-  value = ["${aws_subnet.private_subnet.*.id}"]
+  value = aws_subnet.private_subnet.*.id
 }
 
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
-  value = ["${aws_subnet.public_subnet.*.id}"]
+  value = aws_subnet.public_subnet.*.id
 }
 
 output "nat_gateway_ids" {
   description = "List of NAT Gateway IDs"
-  value = ["${aws_nat_gateway.nat_gateway.*.id}"]
+  value = aws_nat_gateway.nat_gateway.*.id
 }
 
 output "nat_gateway_public_ips" {
   description = "List of NAT Gateway public IP addresses"
-  value = ["${aws_eip.nat_eip.*.public_ip}"]
+  value = aws_eip.nat_eip.*.public_ip
 }
